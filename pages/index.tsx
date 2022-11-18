f12c88a2cd705f708743f341e1db915f81d16cae
Babeloula <walid.benzina@yahoo.fr>
2022-11-18 17:40:53 +0100

    Fix Tailwind CSS setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
@@ -10,18 +9,15 @@ const Home: NextPageWithLayout = () => {
 
   return (
     <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
-      <Image
-        src="/Google.png"
-        alt="Google Logo"
-        width={272}
-        height={92}
-        priority
-      />
       <Search />
       <p>
         Google offered in:{' '}
-        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'}>
-          <a className="underline text-blue-600"> Français</a>
+        <Link
+          href="/"
+          locale={locale === 'en' ? 'fr' : 'en'}
+          className="underline text-blue-600"
+        >
+          Français
         </Link>
       </p>
     </section>
